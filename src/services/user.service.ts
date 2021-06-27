d43ddf6ad21f29adaedb07677426e261a6a779ef
Kiet Pham <kietpham.dev@gmail.com>
2021-06-27 11:07:10 +1000

    Login with Store

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,1 +1,43 @@
-export const userService = {};
+import env from 'react-dotenv';
+
+const login = (email: string, password: string) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password })
+  };
+
+  return fetch(`${env.API_URL}/api/v1/auth`, requestOptions)
+    .then(handleResponse)
+    .then((response) => {
+      sessionStorage.setItem('user', JSON.stringify(response));
+      return response;
+    });
+
+}
+
+const logout = () => {
+  sessionStorage.removeItem('user');
+}
+
+const handleResponse = (response: any) => {
+  return response.text().then((text: string) => {
+    const data = text && JSON.parse(text);
+    if (!response.text) {
+      if (response.status === 401) {
+        logout();
+      }
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+}
+
+export const userService = {
+  login,
+  logout
+}
